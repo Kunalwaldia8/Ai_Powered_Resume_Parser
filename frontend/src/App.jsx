54d5c220f0ed87209697703ffd5a8d4b7190f207
Divyansh Dhaundiyal <112102507+Divyansh1316@users.noreply.github.com>
2025-05-24 17:10:06 +0530

    UI final changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,26 @@
-import ResumeUpload from "./components/ResumeUpload";
 import "./fonts.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Features from "./pages/Features";
+import Process from "./pages/Process";
+import About from "./pages/About";
+import Navbar from "./components/Navbar";
+import { ThemeProvider } from "./components/ThemeContext";
 
-function App() {
-  return <ResumeUpload />;
-}
+const App = () => {
+  return (
+    <div>
+      <ThemeProvider>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/features" element={<Features />} />
+          <Route path="/process" element={<Process />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </ThemeProvider>
+    </div>
+  );
+};
 
 export default App;
